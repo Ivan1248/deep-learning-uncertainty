a6af6b6b532c150f53c7ea1d8a22f46e5c22c299
Ivan <ivan.grubisic.mail@gmail.com>
2018-05-16 20:09:41 +0200

    Changed initialization to variance scaling taking into account only input size

--- dl_uncertainty/tf_utils/variables.py
+++ dl_uncertainty/tf_utils/variables.py
@@ -9,11 +9,11 @@ def conv_weight_variable(ksize,
     if type(ksize) is int:
         ksize = [ksize, ksize]
     shape = list(ksize) + [in_channels, out_channels]
-    n = ksize[0] * ksize[1] * in_channels + out_channels
-    initializer = tf.random_normal_initializer(stddev=np.sqrt(2 / n))
-    var = tf.get_variable(name, shape=shape, initializer=initializer)
+    var = tf.get_variable(
+        name, shape=shape, initializer=tf.variance_scaling_initializer())
     print(var.name)
     return var
+
 
 def vector_variable(n: int, initial_value, name):
     var = tf.get_variable(
@@ -21,6 +21,6 @@ def vector_variable(n: int, initial_value, name):
     print(var.name)
     return var
 
+
 def bias_variable(n: int, initial_value=0.0, name='bias'):
     return vector_variable(n, initial_value, name)
-
