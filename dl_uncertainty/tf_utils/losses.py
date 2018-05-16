7bb16f47efab79078834387729b875ff8d405286
Ivan <ivan.grubisic.mail@gmail.com>
2018-05-16 20:10:14 +0200

    Fixed cross entropy loss

--- dl_uncertainty/tf_utils/losses.py
+++ dl_uncertainty/tf_utils/losses.py
@@ -14,13 +14,44 @@ def multiclass_hinge_loss(labels_oh, logits, delta=1):
 
 def cross_entropy_loss(logits, labels, reduce_mean=True):
     class_count = logits.shape[-1].value
-    labels_oh = tf.one_hot(labels, class_count)
+    labels_oh = tf.one_hot(labels, class_count)  # NHWC -> MC, M=N*H*W
     loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-        logits=logits, labels=labels_oh)
+        logits=logits, labels=labels_oh)  # M
+    loss = loss * tf.cast(tf.greater_equal(labels, 0), tf.float32) # necessary
     label_count = tf.reduce_sum(labels_oh)
     if reduce_mean:
-        return tf.reduce_sum(loss) / label_count  # 1
+        return tf.reduce_sum(loss) / label_count
     return loss * (tf.size(labels) / label_count)  # N
+
+
+def weighted_cross_entropy_loss(logits, labels):
+    class_count = logits.shape[-1].value
+    with tf.name_scope(None, 'CrossEntropyLoss', [logits, labels]):
+        labels = tf.reshape(labels, [-1])
+        onehot_labels = tf.one_hot(labels, class_count)
+        logits = tf.reshape(logits, [-1, class_count])
+        xent = tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits, labels=onehot_labels)
+
+        num_labels = tf.reduce_sum(onehot_labels)
+
+        class_weights = tf.ones([class_count])
+        class_weights = tf.concat([[0], class_weights], axis=0)
+        weights = tf.gather(class_weights, labels + 1)
+
+        xent = tf.multiply(weights, xent)
+        xent = tf.reduce_sum(xent) / num_labels
+
+        myloss = cross_entropy_loss(logits, labels)
+        grad = tf.gradients(xent, [logits])[0]
+        mygrad = tf.gradients(myloss, [logits])[0]
+        xent = tf.cond(
+            tf.equal(xent, myloss), lambda: xent,
+            lambda: tf.Print(xent, [xent, myloss]))
+        xent = tf.cond(
+            tf.reduce_all(tf.equal(grad, mygrad)), lambda: xent,
+            lambda: tf.Print(xent, [(tf.abs(grad - mygrad) / mygrad)]))
+        return xent
 
 
 def cross_entropy_loss_p(probs, labels, reduce_mean=True):
