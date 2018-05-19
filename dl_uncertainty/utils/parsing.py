bce99da0c82fe8de375239178cb420a2b6d42299
Ivan <ivan.grubisic.mail@gmail.com>
2018-05-19 09:20:51 +0200

    Added training curve plotting

---
+++ dl_uncertainty/utils/parsing.py
@@ -0,0 +1,17 @@
+import re
+import numpy as np
+
+def parse_log(log_lines):
+    read = False
+    curves = dict()
+    for line in log_lines:
+        if read:
+            read = False
+            evals = re.findall("(\w+[=\s]\d.\d+)", line, re.IGNORECASE)
+            evals = map(lambda x: re.split('[=\s]', x), evals)
+            for name, value in evals:
+                curves[name] = curves.get(name, []) + [float(value)]
+        else:
+            read = re.search("Testing(?:\.\.\.| \(validation)", line,
+                             re.IGNORECASE) is not None
+    return {k: np.array(v) for k, v in curves.items()}
