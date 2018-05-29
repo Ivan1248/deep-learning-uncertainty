3cc8af5262574cb6c00eb982139901adb387f340
Ivan <ivan.grubisic.mail@gmail.com>
2018-05-29 22:59:42 +0200

    Added max-logit / max-probs out-of-distribution classification evaluation

--- scripts/test_threshold_ood.py
+++ scripts/test_threshold_ood.py
@@ -2,27 +2,29 @@ import argparse
 import datetime
 
 import numpy as np
-from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, aupr
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, auc
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 
 from _context import dl_uncertainty
 
-from dl_uncertainty.data import DataLoader
+from dl_uncertainty.data import DataLoader, datasets
 from dl_uncertainty import dirs, training
 from dl_uncertainty import data_utils, model_utils
 from dl_uncertainty.utils.visualization import view_predictions
 from dl_uncertainty.processing.shape import fill_to_shape
+from dl_uncertainty.processing.data_augmentation import random_crop
 
 # Use "--trainval" only for training on "trainval" and testing "test".
-# CUDA_VISIBLE_DEVICES=0 python train_ood.py
-# CUDA_VISIBLE_DEVICES=1 python train_ood.py
-# CUDA_VISIBLE_DEVICES=2 python train_ood.py
+# CUDA_VISIBLE_DEVICES=0 python test_threshold_ood.py
+# CUDA_VISIBLE_DEVICES=1 python test_threshold_ood.py
+# CUDA_VISIBLE_DEVICES=2 python test_threshold_ood.py
 #   cifar wrn 28 10
-#   cifar dn 100 12
+#   cifar dn 100 12 /home/igrubisic/projects/dl-uncertainty/data/nets/cifar-trainval/dn-100-12-e300/2018-05-28-0121/Model
 #   cifar rn 34 8
 #   cityscapes dn 121 32  /home/igrubisic/projects/dl-uncertainty/data/nets/cityscapes-train/dn-121-32-pretrained-e30/2018-05-16-1623/Model
 #   mozgalo rn 50 64
-#   mozgalo rn 18 64
+#   mozgalo rn 18 64 /home/igrubisic/projects/dl-uncertainty/data/nets/mozgalo-trainval/rn-18-64-e12/2018-05-21-1208/Model
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ds', type=str)
@@ -32,88 +34,130 @@ parser.add_argument('width', type=int)
 parser.add_argument('saved_path', type=str)
 parser.add_argument('--dropout', action='store_true')
 parser.add_argument('--mcdropout', action='store_true')
-parser.add_argument('--trainval', action='store_true')
 parser.add_argument('--test_on_training_set', action='store_true')
 args = parser.parse_args()
 print(args)
 
+
+def myresize(x, shape):
+    x_min, x_max = np.min(x), np.max(x)
+    s = x_max - x_min
+    x = (x - x_min) / s
+    x = resize(x, shape, order=1, clip=False)
+    return s * x + x_min
+
+
 # Cached dataset with normalized inputs
 
 print("Setting up data loading...")
-ds_id_to_dataset = {
+ds_id_to_ds = {
     ds_id: data_utils.get_cached_dataset_with_normalized_inputs(
-        args.ds, trainval_test=args.trainval)[1]
-    for ds_id in ['cifar', 'mozgalo', 'camvid']
+        ds_id, trainval_test=True)[1].subset(np.arange(2000))
+    for ds_id in ['cifar', 'tinyimagenet', 'isun', 'mozgalo', 'cityscapes']
 }
 
-ds = ds_id_to_ds[args.ds]
-del ds_id_to_ds[args.ds]
-
-shape = ds[0][0].shape
-for k, v in ds_id_to_ds:
-    ds_id_to_ds[k] = v.map(lambda x: fill_to_shape(x, shape), 0)
-
-ds_train, ds_test = data_utils.get_cached_dataset_with_normalized_inputs(
-    args.ds, trainval_test=args.trainval)
+shape = ds_id_to_ds[args.ds][0][0].shape
+size = len(ds_id_to_ds[args.ds])
+for ds_id, ds in list(ds_id_to_ds.items()):
+    if ds_id == args.ds:
+        continue
+    if not (args.ds == 'cifar' and ds_id in ['isun', 'mozgalo']):
+        ds_id_to_ds[ds_id + '-crop'] = \
+            ds.map(lambda d: random_crop(d, shape[:2]), 0, func_name='crop')
+    ds_id_to_ds[ds_id + '-res'] = \
+        ds.map(lambda d: myresize(d, shape[:2]), 0, func_name='resize')
+    del ds_id_to_ds[ds_id]
+ds_id_to_ds['gaussian'] = \
+    datasets.WhiteNoiseDataset(shape, size=size).map(lambda x: (x, -1))
+ds_id_to_ds['uniform'] = datasets.WhiteNoiseDataset(
+    shape, size=size, uniform=True).map(lambda x: (x, -1))
 
 # Model
 
 print("Initializing model and loading state...")
 model = model_utils.get_model(
     net_name=args.net,
-    ds_train=ds_train,
+    ds_train=ds_id_to_ds[args.ds],
     depth=args.depth,
-    width=args.width,  # width factor for WRN, base_width for others
-    epoch_count=args.epochs,
-    dropout=args.dropout or args.mcdropout,
-    pretrained=args.pretrained)
+    width=args.width,
+    epoch_count=1,
+    dropout=args.dropout or args.mcdropout)
 model.load_state(args.saved_path)
 
 
-def get_misclassification_example(xy):
-    x, y = xy
-    logits, output = model.predict(x, outputs=['logits', 'output'])
-    return logits, int(output != y)
-
-
-def to_logit_out_distribution_label(xy):
-    logits = model.predict(xy[0], outputs=['logits'])
-    return logits, 1
-
-
-def to_logit_in_distribution_label(xy):
-    logits = model.predict(xy[0], outputs=['logits'])
-    return logits, 0
+def to_logits(xy, label=0):
+    x, _ = xy
+    logits, output = model.predict(
+        x, single_input=True, outputs=['logits', 'output'])
+    return logits, label  #int(output != y)
 
 
 ds_id_to_logits_ds = {
-    k: v.map(get_misclassification_example)
+    k: v.map(
+        lambda x: to_logits(x, label=int(k == args.ds)),
+        func_name='to_logits').cache()
     for k, v in ds_id_to_ds.items()
 }
-
-logits_ds_test = ds_test.map(get_misclassification_example)
-
-max_logits_ds_test = np.array([y for x, y in logits_ds_test])
 
 ds_id_to_max_logits_ds = {
     k: v.map(lambda xy: (np.max(xy[0]), xy[1]))
     for k, v in ds_id_to_logits_ds.items()
 }
 
+ds_id_to_logits_sum_ds = {
+    k: v.map(lambda xy: (np.sum(xy[0]), xy[1]))
+    for k, v in ds_id_to_logits_ds.items()
+}
 
-def max_logit_ood_classifier(threshold):
-    return lambda x: np.max(x) < threshold
-
-
-max_logits_range = (np.min(max_logits_ds_test), np.max(max_logits_ds_test))
+max_logits_ds = ds_id_to_max_logits_ds[args.ds]
+max_logits_range = (np.min(max_logits_ds), np.max(max_logits_ds))
 thresholds = np.linspace(*max_logits_range, 50)
 
-y_true_in, y_score_in = zip(* [(x, y) for x, y in max_logits_ds_test.items()])
+y_score_in, y_true_in = zip(* [(x, y) for x, y in max_logits_ds])
 
-for ds_id, ds in ds_id_to_max_logits_ds.items():
-    y_true_out, y_score_out = zip(* [(x, y) for x, y in ds.items()])
+fig, axes = plt.subplots(
+    3,
+    len(ds_id_to_max_logits_ds),
+    figsize=(40, 16),
+    sharex=False,
+    sharey='row')
+
+for i, (ds_id, ds) in enumerate(ds_id_to_max_logits_ds.items()):
+    y_score_out, y_true_out = zip(* [(x, y) for x, y in ds])
     y_true = y_true_in + y_true_out
     y_score = y_score_in + y_score_out
 
     fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_score)
-    plt.plot(fpr, tpr)
+
+    ax = axes[0, i]
+    ax.set_title(ds_id)
+    r = (min(y_score), max(y_score))
+    ax.hist(y_score_in, bins=40, range=r, alpha=0.5)
+    ax.hist(y_score_out, bins=40, range=r, alpha=0.5)
+
+    ax = axes[1, i]
+    ax.plot(fpr, fpr, linestyle='--')
+    ax.plot(fpr, tpr, label=f'{auc(fpr, tpr):.3f}')
+    ax.set_xlabel('FPR')
+    if i == 0:
+        ax.set_ylabel('TPR')
+    ax.legend()
+
+    ax = axes[2, i]
+    p, r, thresholds = precision_recall_curve(
+        y_true=y_true, probas_pred=y_score)
+    pi = np.copy(p)
+    for i, v in enumerate(p):
+        pi[i] = np.max(p[:i + 1])
+    ax.plot(r, p, label=f'{auc(r, p):.3f}')
+    ax.plot(r, pi, label=f'{auc(r, pi):.3f}')
+    ax.set_xlabel('R')
+    if i == 0:
+        ax.set_ylabel('P')
+    ax.legend()
+#fig.tight_layout(pad=0.1)
+plt.title(args.ds)
+plt.tight_layout(pad=0.5)
+plt.xticks(fontsize=8)
+plt.xticks(fontsize=8)
+plt.show()
